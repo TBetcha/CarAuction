3b478107b61939089c0d4c4498e417baeaf8b403
Tbetcha <troy.boettger@gmail.com>
2026-04-13 00:13:30 -0400

    add search controller and some formatting

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,8 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
 
 namespace SearchService;
 
-public class SearchController: ControllerBase
+[ApiController]
+[Route("api/search")]
+
+public class SearchController : ControllerBase
 {
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    {
+        var query = DB.Find<Item>();
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+        return result;
+
+    }
 
 }
--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -6,26 +6,26 @@ namespace SearchService;
 
 public class DbInitializer
 {
-  public static async Task InitDb(WebApplication app) 
-  {
-
-    await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
-
-    await DB.Index<Item>()
-      .Key(x => x.Make, KeyType.Text)
-      .Key(x => x.Model, KeyType.Text)
-      .Key(x => x.Color, KeyType.Text)
-      .CreateAsync();
-
-    var count = await DB.CountAsync<Item>();
-
-    if(count == 0)
+    public static async Task InitDb(WebApplication app)
     {
-      Console.WriteLine("No data - will attempt to seed");
-      var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-      var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
-      var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-      await DB.SaveAsync(items);
+
+        await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+          .Key(x => x.Make, KeyType.Text)
+          .Key(x => x.Model, KeyType.Text)
+          .Key(x => x.Color, KeyType.Text)
+          .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        if (count == 0)
+        {
+            Console.WriteLine("No data - will attempt to seed");
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+            await DB.SaveAsync(items);
+        }
     }
-  }
 }
