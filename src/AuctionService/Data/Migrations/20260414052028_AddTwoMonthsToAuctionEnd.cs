26ab08e93f53fb1ef16610e8aad3959edcfb41d5
Tbetcha <troy.boettger@gmail.com>
2026-04-14 01:36:47 -0400

    fix data so no auctions are expired yet

---
+++ src/AuctionService/Data/Migrations/20260414052028_AddTwoMonthsToAuctionEnd.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTwoMonthsToAuctionEnd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            UPDATE ""Auctions""
+            SET ""AuctionEnd"" = ""AuctionEnd"" + INTERVAL '2 months'
+            ");
+        }
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            UPDATE ""Auctions""
+            SET ""AuctionEnd"" = ""AuctionEnd"" - INTERVAL '2 months'
+            ");
+        }
+    }
+}
